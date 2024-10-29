78b4e6e7eb2b00e85f23936c5711ff5fe6349b06
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-29 17:16:59 -0500

    style: extend tailwind properties

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Zain'],        
+        serif: ['Gluten'],    
+        mono: ['Darumadrop One'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        darkBlue: "#0B1734",
+        deepOrange: "#EF6125",
+        deepGreen: "#6bbd48",
+        lightGreen: "#D2EBA8",
+        lightOrange: "#FAD7B4",
+      },
+      width: {
+        '1/7': '14.2857143%',
+        '1/8': '12.5%',
+        '1/9': '11.1111111%',
+        '1/10': '10%',
+        '2/7': '28.5714286%',
+        '2/5': '40%',
+        '3/7': '42.8571429%',
+        '4/7': '57.1428571%',
+        '5/7': '71.4285714%',
+        '6/7': '85.7142857%',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
