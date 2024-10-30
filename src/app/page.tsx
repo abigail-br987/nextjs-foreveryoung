553747295debbbfa75edf28442bf130ba959c2e2
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-30 07:53:16 -0500

    feat: content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,23 +9,11 @@ export default function Home() {
     <>
       <AnimatedLanding />
       <section className="bg-deepGreen border-2 border-black z-40">
-        <div className="max-w-screen-2xl p-8 m-auto">
-          <div className="">
-            <p>
+        <div className="max-w-screen-2xl p-8 m-auto space-y-10">
+          <div className="text-center">
+            <p className="text-2xl">
               Somos la <strong>organización de voluntariado</strong> del Servicio de Medicina del
-              Adolescente del Hospital del Niño de Breña, Lima, Perú. Estamos
-              inscritos en el Registro Nacional de Organizaciones juveniles.
-            </p>
-            <p>
-              Esta experiencia se desarrolla en el marco de los derechos que
-              tienen los jóvenes a la <strong>SALUD, EDUCACIÓN Y PARTICIPACIÓN</strong> , con un
-              enfoque de equidad, de derecho y diversidad. Trabajamos con los
-              profesionales del equipo multidisciplinario en actividades
-              preventivas de conductas de riesgo y de promoción de estilos de
-              vida saludables con sus pares. Sistema organizado de adolescentes,
-              que trabajan voluntariamente en el desarrollo de actividades
-              dirigidos a adolescentes. Cada adolescente al formar parte de
-              ForeverYoung, contribuye al desarrollo del servicio
+              Adolescente del Hospital del Niño de Breña, Lima, Perú.
             </p>
           </div>
           <WhatDoWeDo />
