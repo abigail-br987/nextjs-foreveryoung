d6e0c54726a3b5f785f1df91b4b43009a57ecb17
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-31 06:37:23 -0500

    style, format

--- src/components/minicomponents/ParticipantCard.jsx
+++ src/components/minicomponents/ParticipantCard.jsx
@@ -1,20 +1,26 @@
 const PersonCard = ({ name, title, description, imageSrc }) => {
   return (
-    <div className="flex items-center border-2 border-dark_blue rounded-lg bg-white ">
+    <div className="items-center bg-darkBlue border-2 border-darkBlue rounded-lg  ">
+      
+      <div className="mx-5 hidden sm:block text-center uppercase text-white">
+        <h2>
+          {title}
+        </h2>
+      </div>
       <div>
         <h2 className="text-center block sm:hidden my-2">{title}</h2>
         <img
           src={imageSrc}
           alt={name}
-          className="h-full aspect-square sm:max-w-40 object-cover border-dark_blue max-sm:border-t-2 sm:border-r-2 rounded-sm"
+          className="h-full aspect-square
+           object-cover border-dark_blue max-sm:border-t-2 sm:border-r-2 rounded-lg"
         />
       </div>
-
-      <div className="mx-5 hidden sm:block">
-        <h2>
-          {name} : {title}
+    
+      <div className="mx-5 hidden sm:block text-center uppercase text-white">
+      <h2>
+          {name}
         </h2>
-        <p>{description}</p>
       </div>
     </div>
   );
