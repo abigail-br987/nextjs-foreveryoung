41cd9d75c50852b9c96db3d1f60d67e14908bcf5
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-31 08:36:50 -0500

    feat: lyout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,48 +8,42 @@ import Footer from "@/components/sections/Footer";
 import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
 import AnimatedDiv from "@/components/minicomponents/AnimatedDiv";
-// Define animations for entrance and exit
-
+import { useViewportScroll, useTransform } from "framer-motion";
+import { StarIcon } from "@/components/figures/StarIcon";
 
 export default function Home() {
-
-
   return (
     <>
       <AnimatedLanding />
 
-      <div className="bg-darkBlue"> 
-      <AnimatedDiv
-        className="bg-deepGreen border-2 border-black z-40"
-      >
-        <div className="max-w-screen-2xl p-8 m-auto space-y-16">
-          <div className="text-center">
-            <p className="text-2xl"> 
-              Somos la <strong>organización de voluntariado</strong> del Servicio de Medicina del
-              Adolescente del Hospital del Niño de Breña, Lima, Perú.
-            </p>
+      <div className="bg-darkBlue">
+        <AnimatedDiv className="bg-deepGreen border-2 border-black z-30">
+          <div className="max-w-screen-2xl p-8 m-auto space-y-16">
+            <div className="text-center">
+              <p className="text-2xl">
+                Somos la <strong>organización de voluntariado</strong> del
+                Servicio de Medicina del Adolescente del Hospital del Niño de
+                Breña, Lima, Perú.
+              </p>
+            </div>
+            <WhatDoWeDo />
           </div>
-          <WhatDoWeDo />
-        </div>
-      </AnimatedDiv>
+        </AnimatedDiv>
 
-      <AnimatedDiv
-        className="bg-white border-2 border-black z-40"
-      >
-        <div className="max-w-screen-2xl p-8 m-auto space-y-16">
-          <WhoAreWe />
-        </div>
-      </AnimatedDiv>
+        <AnimatedDiv className="bg-white border-2 border-black z-30">
+          <div className="max-w-screen-2xl p-8 m-auto space-y-16">
+            <WhoAreWe />
+          </div>
+        </AnimatedDiv>
 
-      <AnimatedDiv
-        className="bg-deepOrange border-2 border-black z-40"
-      >
-        <div className="max-w-screen-2xl p-8 m-auto space-y-16">
-          <JoinUs />
-        </div>
-      </AnimatedDiv>
+        <AnimatedDiv className="bg-deepOrange border-2 border-black z-30">
+          <div className="max-w-screen-2xl p-8 m-auto space-y-16">
+            <JoinUs />
+          </div>
+        </AnimatedDiv>
 
-      <Footer />
-      </div></>
+        <Footer />
+      </div>
+    </>
   );
 }
