faddbab338e44e43cb891194561ef9574f3186c8
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-31 07:34:39 -0500

    feat: animated component

---
+++ src/components/minicomponents/AnimatedDiv.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import { useInView } from "framer-motion";
+interface AnimatedDivProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const AnimatedDiv = ({ children, className = "" }: AnimatedDivProps) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(ref, { once: false, margin: "-5% 0px" });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ scale: 0.97 }}
+      animate={{ 
+        scale: isInView ? 1 : 0.97, 
+      }}
+      transition={{ 
+        duration: 0.5,
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+      }}
+      className={className}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default AnimatedDiv;
