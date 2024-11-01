7f0e6c43f73d79651fed0457dfca1b6831098f94
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-11-01 17:03:39 -0500

    style, add links

--- src/components/parts/JoinUs.tsx
+++ src/components/parts/JoinUs.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { HoverImageCard } from "./WhatDoWeDo";
+import { InfoCard } from "./WhatDoWeDo";
 const joinus = [
   {
     title: "🤝 Como Alianza",
@@ -9,28 +12,45 @@ const joinus = [
     title: "🌟 Como Miembro",
     description:
       "Sé parte activa de nuestras iniciativas. Para participar, debes pasar por Servicio de Medicina del Adolescente y preguntar a la Lic. Katty Rojas de Consejería de Salud Sexual y Reproductiva sobre la posibilidad de unirse.",
-    image: "/images/photo14.jpg",
+    image: "/images/photo2.jpeg",
   },
 ];
 
 function JoinUs() {
+
   return (
     <div className="space-y-4">
-      <h1 className="mb-2 ">Únete a ForeverYoung</h1>
+      <h1 className="mb-2">Únete a ForeverYoung</h1>
       <div className="grid md:grid-cols-2 gap-2 justify-center">
         {joinus.map((item, index) => (
-          <div key={index} className="rounded-lg">
-            <div className="w-full h-96 border-2 border-darkBlue rounded-lg">
-              <img
-                src={item.image}
-                alt={`Image for ${item.title}`}
-                className="w-full h-full object-cover object-top rounded-lg"
-              />
-            </div>
-            <div className="p-3">
+          <div key={index}>
+            <div
+              className={`w-full space-y-2 rounded-lg `}
+            >
+             <div className="p-8  text-darkBlue bg-white border-2 rounded-lg border-darkBlue shadow-sm shadow-darkBlue">
               <h2 className="mt-2">{item.title}</h2>
               <p>{item.description}</p>
+            </div> 
+
+              <div
+                className={`flex items-start border-2 border-darkBlue rounded-lg `}
+              >
+
+                <div
+                  className="w-full h-96 rounded-lg
+                  bg-cover bg-center cursor-pointer overflow-hidden transition-all relative"
+                  onClick={() => window.open("http://hola.com", "_blank")}
+                >
+                  <div
+                    className="absolute h-96 inset-0 bg-cover bg-center transition-transform hover:rotate-1 duration-300 ease-in-out hover:scale-110"
+                    style={{
+                      backgroundImage: `url(${item.image})`,
+                    }}
+                  />
+                </div>
+              </div>
             </div>
+
           </div>
         ))}
       </div>
