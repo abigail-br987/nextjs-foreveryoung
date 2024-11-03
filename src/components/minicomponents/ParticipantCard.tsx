0345e53254fcf02aa34ad0ab409d9930cac72221
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-11-03 11:13:48 -0500

    style,

--- src/components/minicomponents/ParticipantCard.tsx
+++ src/components/minicomponents/ParticipantCard.tsx
@@ -1,25 +1,22 @@
-const PersonCard = ({ name, title, description, imageSrc }) => {
+const PersonCard = ({ name, title, imageSrc }) => {
   return (
-    <div className="items-center shadow-sm flex-col flex
-     justify-center shadow-darkBlue border-2 bg-white border-darkBlue rounded-lg
-      text-darkBlue pb-2 ">
+    <div className="items-center shadow-sm flex-col flex justify-center shadow-darkBlue border-2 bg-white border-darkBlue rounded-lg text-darkBlue pb-2">
       
-      <div className="p-4 ">
+      <div className="p-4">
         <h2 className="text-center block sm:hidden my-2">{title}</h2>
         <img
           src={imageSrc}
           alt={name}
-          className="h-full aspect-square
-           object-cover border-darkBlue border-2 rounded-lg"
+          className="aspect-square object-cover border-darkBlue border-2 rounded-lg"
         />
       </div>
 
-        <h2>
-          {title}
-        </h2>
-        <h3>
+      <h2>
+        {title}
+      </h2>
+      <h3>
         {name}
-        </h3>
+      </h3>
 
     </div>
   );
