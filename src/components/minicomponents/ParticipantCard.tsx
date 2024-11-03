4e37169065023b400e118a02d386ba4ef151f622
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-11-03 16:48:16 -0500

    debug: unused variables

--- src/components/minicomponents/ParticipantCard.tsx
+++ src/components/minicomponents/ParticipantCard.tsx
@@ -1,7 +1,14 @@
-const PersonCard = ({ name, title, imageSrc }) => {
+import React from "react";
+
+interface PersonCardProps {
+  name: string;
+  title: string;
+  imageSrc: string;
+}
+
+const PersonCard: React.FC<PersonCardProps> = ({ name, title, imageSrc }) => {
   return (
     <div className="items-center shadow-sm flex-col flex justify-center shadow-darkBlue border-2 bg-white border-darkBlue rounded-lg text-darkBlue pb-2">
-      
       <div className="p-4">
         <h2 className="text-center block sm:hidden my-2">{title}</h2>
         <img
@@ -10,14 +17,8 @@ const PersonCard = ({ name, title, imageSrc }) => {
           className="aspect-square object-cover border-darkBlue border-2 rounded-lg"
         />
       </div>
-
-      <h2>
-        {title}
-      </h2>
-      <h3>
-        {name}
-      </h3>
-
+      <h2>{title}</h2>
+      <h3>{name}</h3>
     </div>
   );
 };
