45dab9e5f94675096bcdfc979bc636b81d95f217
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-11-03 11:13:09 -0500

    new page: nosotros

---
+++ src/app/nosotros/page.tsx
@@ -0,0 +1,16 @@
+import AnimatedDiv from "@/components/minicomponents/AnimatedDiv"
+import WhoAreWe from "@/components/parts/WhoAreWe"
+import NavBar from "@/components/sections/NavBar"
+function Nosotros () {
+    return (
+        <div className="bg-white flex flex-col items-center justify-center">       
+        <AnimatedDiv>
+        <div className="max-w-screen-2xl px-8 py-12">
+          <WhoAreWe />
+        </div>
+      </AnimatedDiv>
+      </div>
+    )
+}
+
+export default Nosotros
