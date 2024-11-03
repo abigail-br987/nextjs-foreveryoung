17a954585ab392ba27834b2b760b703568a7f4fe
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-11-03 09:37:25 -0500

    feat: contact component

---
+++ src/app/contacto/page.tsx
@@ -0,0 +1,16 @@
+import NavBar from "@/components/sections/NavBar";
+import Contact from "@/components/sections/ContactForm";
+
+const Contacto = () => {
+  return (
+    <div className="bg-deepGreen"> 
+    <NavBar/>
+    <div className="min-h-screen m-auto flex justify-center items-center
+      text-white max-w-screen-lg">
+        <Contact />
+    </div>
+    </div>
+  );
+};
+
+export default Contacto;
