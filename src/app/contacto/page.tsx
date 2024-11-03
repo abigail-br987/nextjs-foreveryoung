beb8d3ce3ce3e2e0803257486834768c9fd37a98
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-11-03 16:24:42 -0500

    style

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -4,7 +4,7 @@ import Contact from "@/components/sections/ContactForm";
 const Contacto = () => {
   return (
     <div className="bg-deepGreen"> 
-    <div className="min-h-screen m-auto flex justify-center items-center
+    <div className="min-h-[91vh] m-auto flex justify-center items-center
       text-white max-w-screen-lg">
         <Contact />
     </div>
