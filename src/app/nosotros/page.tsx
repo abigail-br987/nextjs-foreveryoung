3c6ff9ef558f125b1619e47fde353b88bdb72d0d
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-11-06 14:09:30 -0500

    style: add stars

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -1,15 +1,19 @@
-import AnimatedDiv from "@/components/minicomponents/AnimatedDiv"
-import WhoAreWe from "@/components/parts/WhoAreWe"
-function Nosotros () {
-    return (
-        <div className="bg-white flex flex-col items-center justify-center">       
-        <AnimatedDiv>
-        <div className="max-w-screen-2xl px-8 py-12">
+import AnimatedDiv from "@/components/minicomponents/AnimatedDiv";
+import WhoAreWe from "@/components/parts/WhoAreWe";
+import { StarIcon } from "@/components/figures/StarIcon";
+function Nosotros() {
+  return (
+    <div className="bg-white flex flex-col items-center justify-center">
+        <AnimatedDiv className="max-w-screen-2xl px-8 py-12">
           <WhoAreWe />
-        </div>
-      </AnimatedDiv>
-      </div>
-    )
+        </AnimatedDiv>
+
+        <StarIcon className="absolute text-deepOrange right-[6%] top-20" />
+        <StarIcon className="absolute text-deepGreen right-[1%] bottom-10" />
+        <StarIcon className="absolute text-darkBlue left-[0%] bottom-[36%]" />
+ 
+    </div>
+  );
 }
 
-export default Nosotros
+export default Nosotros;
