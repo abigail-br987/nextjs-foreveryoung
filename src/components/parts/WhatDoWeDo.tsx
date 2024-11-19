79d33f1af50981b06b0a516f659ab98f03f0b41a
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-11-19 16:44:52 -0500

    style: order

--- src/components/parts/WhatDoWeDo.tsx
+++ src/components/parts/WhatDoWeDo.tsx
@@ -17,11 +17,11 @@ export const HoverImageCard: React.FC<HoverImageCardProps> = ({
       className={`relative flex items-start border-2 border-darkBlue rounded-lg ${className}`}
     >
       <div
-        className="w-full h-full rounded-lg bg-cover bg-center cursor-pointer overflow-hidden transition-all relative"
+        className="w-full h-80 rounded-lg bg-cover bg-center cursor-pointer overflow-hidden transition-all relative"
         onClick={() => window.open(link, "_blank")}
       >
         <div
-          className="absolute inset-0 bg-cover bg-center transition-transform duration-300 ease-in-out hover:scale-110"
+          className="absolute h-80 inset-0 bg-cover bg-center transition-transform duration-300 ease-in-out hover:scale-110"
           style={{
             backgroundImage: `url(${backgroundImage})`,
           }}
@@ -49,9 +49,11 @@ export const InfoCard: React.FC<InfoCardProps> = ({
       className={`p-5 relative rounded-lg border-2
          border-darkBlue bg-white flex flex-col ${className}`}
     >
-      <div className="absolute -top-3 -left-3 flex items-center 
+      <div
+        className="absolute -top-3 -left-3 flex items-center 
       justify-center bg-white p-1 text-2xl
-       border-2 aspect-square border-darkBlue rounded-full">
+       border-2 aspect-square border-darkBlue rounded-full"
+      >
         {icon}
       </div>
       <h2 className="text-center">{title}</h2>
@@ -79,89 +81,78 @@ function WhatDoWeDo() {
         adolescentes. Cada adolescente al formar parte de ForeverYoung,
         contribuye al desarrollo del servicio.
       </p>
-      <div className="sm:grid max-sm:space-y-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-rows-4 w-full max-w-full gap-2 rounded-lg">
-        {/* TALLERES */}
+      <div className="sm:grid max-sm:space-y-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-full gap-2 rounded-lg">
+
         <InfoCard
           icon={<BiBulb />}
           title="Talleres y Charlas Interactivas"
           content="En la sala de espera, se ofrecen talleres y charlas dinámicas que informan e inspiran a los jóvenes sobre su salud y bienestar."
-          className="row-start-1"
-        />
-
-        <HoverImageCard
-          className="max-sm:hidden row-start-1"
-          link="https://www.instagram.com/p/DBCgx7bB-cB/?img_index=1"
-          backgroundImage="/images/taller1.PNG"
-        />
-        <HoverImageCard
-          className="max-md:hidden row-start-1"
-          link="https://www.instagram.com/p/C-GiFPOBM8c/?img_index=3"
-          backgroundImage="/images/taller2.PNG"
-        />
-        <HoverImageCard
-          className="max-lg:hidden row-start-1"
-          link="https://www.instagram.com/p/DBCdcXvhJff/?img_index=1"
-          backgroundImage="/images/taller3.PNG"
-        />
-
-        {/* CONCIENTIZACION */}
-        <HoverImageCard
-          className="max-sm:hidden row-start-2"
-          link="https://www.instagram.com/forever_young_voluntariado/"
-          backgroundImage="/images/screenshot2.png"
-        />
-        <InfoCard
-          icon={<BiWorld />}
-          title="Concientización en Redes Sociales"
-          content="Se realizan campañas de concientización en días especiales, promoviendo mensajes de salud y bienestar para los adolescentes."
-          className="row-start-2"
-        />
-        <HoverImageCard
-          className="max-md:hidden lg:col-span-2 row-start-2"
-          link="https://www.instagram.com/forever_young_voluntariado/"
-          backgroundImage="/images/screenshot1.jpg"
-        />
-
-        {/* PROYECTOS */}
-        <HoverImageCard
-          className="max-sm:hidden md:col-span-2 sm:col-start-2 row-start-3"
-          link="https://www.instagram.com/p/C98hzqzNGyy/?img_index=1"
-          backgroundImage="/images/proyectos1.PNG"
+          className=""
         />
         <InfoCard
           icon={<BiSolidParty />}
           title="Proyectos Creativos"
           content="Se desarrollan proyectos nuevos que incluyen concursos y grupos, fomentando la creatividad y el compromiso en la comunidad."
-          className="sm:col-start-1 md:col-start-3 row-start-3"
+          className=""
         />
-        <HoverImageCard
-          className="max-lg:hidden sm:col-span-1"
-          link="https://www.instagram.com/p/DAOf6iBx2Sf/?img_index=1"
-          backgroundImage="/images/proyectos2.PNG"
-        />
-
-        {/* AFICHES */}
-        <HoverImageCard
-          className="max-md:hidden sm:col-start-2 md:col-start-1 row-start-4 lg:col-start-1"
-          link="https://www.instagram.com/stories/highlights/17842835370287172/"
-          backgroundImage="/images/afiche3.jfif"
-        />
-        <HoverImageCard
-          className="max-sm:hidden md-col-start-3 md:row-start-4 lg:col-start-2"
-          link="https://www.instagram.com/stories/highlights/17842835370287172/"
-          backgroundImage="/images/afiche2.jfif"
-        />
-        <HoverImageCard
-          className="md:col-start-2 max-md:hidden lg:col-start-3"
-          link="https://www.instagram.com/stories/highlights/17842835370287172/"
-          backgroundImage="/images/afiche1.jfif"
+        <InfoCard
+          icon={<BiWorld />}
+          title="Concientización en Redes Sociales"
+          content="Se realizan campañas de concientización en días especiales, promoviendo mensajes de salud y bienestar para los adolescentes."
+          className=""
         />
         <InfoCard
           icon={<BiUserVoice />}
           title="Afiches y Difusión"
           content="Se crea y distribuye información importante mediante afiches en las paredes del hospital y en las redes sociales, asegurando mayor impacto."
-          className="md:col-start-2 md:row-start-4 lg:col-start-4"
+          className=""
         />
+
+     </div>
+
+     <div className="sm:grid max-sm:space-y-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  w-full max-w-full gap-2 rounded-lg">
+
+        <HoverImageCard
+          className=""
+          link="https://www.instagram.com/p/DBCgx7bB-cB/?img_index=1"
+          backgroundImage="/images/taller1.PNG"
+        />
+        <HoverImageCard
+          className=""
+          link="https://www.instagram.com/p/C-GiFPOBM8c/?img_index=3"
+          backgroundImage="/images/taller2.PNG"
+        />
+        <HoverImageCard
+          className=""
+          link="https://www.instagram.com/p/DBCdcXvhJff/?img_index=1"
+          backgroundImage="/images/taller3.PNG"
+        />
+  
+  <HoverImageCard
+          className="max-lg:hidden"
+          link="https://www.instagram.com/p/DAOf6iBx2Sf/?img_index=1"
+          backgroundImage="/images/proyectos2.PNG"
+        />
+
+
+        <HoverImageCard
+          className="max-sm:hidden md:col-span-2"
+          link="https://www.instagram.com/p/C98hzqzNGyy/?img_index=1"
+          backgroundImage="/images/proyectos1.PNG"
+        />
+ 
+        <HoverImageCard
+          className="max-md:hidden"
+          link="https://www.instagram.com/stories/highlights/17842835370287172/"
+          backgroundImage="/images/afiche3.jfif"
+        />
+
+        <HoverImageCard
+          className="max-md:hidden"
+          link="https://www.instagram.com/stories/highlights/17842835370287172/"
+          backgroundImage="/images/afiche1.jfif"
+        />
+
       </div>
     </div>
   );
